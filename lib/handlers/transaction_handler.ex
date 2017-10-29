6deae43941b9aa3235dc74a55db7b922b0c5cfbf
Ilkka Poutanen <ilkka@ilkka.io>
2017-10-29 10:22:24 +0200

    Rewrite to not use unsafe assignment of req

--- lib/handlers/transaction_handler.ex
+++ lib/handlers/transaction_handler.ex
@@ -8,13 +8,16 @@ defmodule Handlers.TransactionHandler do
   def handle(%{:method => "POST"} = req, state) do
     Logger.info("Handling transaction POST request")
 
-    if :cowboy_req.has_body(req) do
-      {:ok, body, req} = :cowboy_req.read_body(req)
-      new_tx = Poison.decode!(body)
-      Logger.info(inspect(new_tx))
-    end
+    reply =
+      if :cowboy_req.has_body(req) do
+        {:ok, body, req} = :cowboy_req.read_body(req)
+        new_tx = Poison.decode!(body)
+        Logger.info(inspect(new_tx))
+        :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "morjesta", req)
+      else
+        :cowboy_req.reply(400, %{"content-type" => "text/plain"}, "not a transaction", req)
+      end
 
-    reply = :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "morjesta", req)
     {:ok, reply, state}
   end
 
