ba21dc5301f325a978044fee811d1e90a679be4b
Ilkka Poutanen <ilkka@ilkka.io>
2017-10-29 10:16:20 +0200

    Don't barf if tx has no body

--- lib/handlers/transaction_handler.ex
+++ lib/handlers/transaction_handler.ex
@@ -7,9 +7,11 @@ defmodule Handlers.TransactionHandler do
 
   def handle(%{:method => "POST"} = req, state) do
     Logger.info "Handling transaction POST request"
-    {:ok, body, req} = :cowboy_req.read_body(req)
-    new_tx = Poison.decode!(body)
-    Logger.info inspect(new_tx)
+    if :cowboy_req.has_body(req) do
+      {:ok, body, req} = :cowboy_req.read_body(req)
+      new_tx = Poison.decode!(body)
+      Logger.info inspect(new_tx)
+    end
     reply = :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "morjesta", req)
     {:ok, reply, state}
   end
